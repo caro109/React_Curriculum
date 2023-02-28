9674cf6cffab61bc232ab6411a52cc1448f3314a
Caro109 <carogarcia.0419@gmail.com>
2023-02-28 15:45:44 -0500

    agregando cambios nuevos

--- src/components/ui/Hooks.jsx
+++ src/components/ui/Hooks.jsx
@@ -1,22 +1,31 @@
-import React from "react";
+import {useState,useEffect} from "react";
 
 export const Hooks = () =>{
 
+  const [characters, setcharacters] = useState("")
+  const [changeStyle, setchangeStyle] = useState('charactersColor')
 
+  useEffect(() => {
+    characters === 255 ? setchangeStyle ('charactersColor2') : setchangeStyle ('charactersColor');
+  }, [characters])
+  
+  const clickCharacters = (e) =>{
+    setcharacters(e.target.value)
 
+  }
+  const sd = characters <=0 
   return(
     <div className="tweetParentContainer">
       <h1>Tweet Generator</h1>
       <div className="containerTweet">
       <h2>Post your tweet</h2>
       <hr/>
-      <textarea name="" id="" cols="30" rows="10" placeholder="write a tweet(max 255 charactes)"></textarea>
+      <textarea name="" id="" cols="30" rows="10" placeholder="write a tweet(max 255 charactes)" onChange={clickCharacters} disabled={sd}></textarea>
       <div className="buttonContainer">
       <button className="buttonTweet">Publicar</button>
-      <button className="buttonTweet">Archivar</button>
       <button className="buttonTweet">Mostrar Archivados</button>
       </div>
-      <p className="numberTweet">255</p>
+      <p className={changeStyle}>{255-characters.length}</p>
       </div>
     </div>
   );
